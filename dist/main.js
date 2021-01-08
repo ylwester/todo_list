da77f1943a773108986df37ae86a836d40f72b46
Sylwester Miecznikowski <sylwester.miecznikowski@gmail.com>
2021-01-08 20:56:54 +0100

    project start

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("chuj")
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
